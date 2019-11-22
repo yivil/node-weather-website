526c5bce686677f2a966640ec3b888aede8d6376
valerieluo <valuo@deloitte.ca>
2019-11-22 03:31:53 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/d363494820450ec516ffab3cf1ab2b1f/' + latitude + ',' + longitude + '?units=si'
+    
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service.', undefined)
+        } else if (body.error) {
+            callback('Unable to find location.', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability * 100 + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
