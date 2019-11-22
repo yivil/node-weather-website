b04cd3237096a88b3414e12c0c6a89b446d5ac11
valerieluo <valuo@deloitte.ca>
2019-11-22 04:44:59 -0500

    Added more weather forecast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,13 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability * 100 + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary 
+                + ' It is currently ' + body.currently.temperature + ' degrees out. Feels like ' 
+                + body.currently.apparentTemperature + ' degrees with wind speed of '
+                + body.currently.windSpeed + ' km/h. There is a ' 
+                + body.currently.precipProbability * 100 + '% chance of rain. The high today will be ' 
+                + body.daily.data[0].temperatureHigh + ' degrees and the low will be ' 
+                + body.daily.data[0].temperatureLow + ' degrees.')
         }
     })
 }
